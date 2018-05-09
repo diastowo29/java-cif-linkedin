51366edd0f2fed0eefbf974668b3e749c732f791
diastowo29 <diastowo@gmail.com>
2018-05-09 17:07:48 +0700

    add getToken and Submit token

--- src/main/java/com/example/Linkedin.java
+++ src/main/java/com/example/Linkedin.java
@@ -1,25 +1,43 @@
 package com.example;
 
+import java.util.HashMap;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @SpringBootApplication
-@RequestMapping("/linkedin")
+@RequestMapping("/linkedin/")
 public class Linkedin {
-	
+	String returnUrl = "";
+
 	@RequestMapping(method = RequestMethod.GET)
-	String index() {
+	String indexGet() {
 		System.out.println("GET LINKEDIN");
-		return "index";
+		return "admin";
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
-	String indexGet() {
+	String indexPost(@RequestBody HashMap<String, String> jsonParam) {
 		System.out.println("POST LINKEDIN");
-		return "index";
+		returnUrl = jsonParam.get("return_url");
+		return "admin";
+	}
+
+	@RequestMapping("/getToken/")
+	String getToken() {
+		System.out.println("GET GETTOKEN");
+		return "getToken";
+	}
+
+	@RequestMapping("/submit")
+	String submitToken(@RequestParam("token") String token) {
+		System.out.println("GET SUBMIT TOKEN: " + token);
+		return "getToken";
 	}
 
 }
