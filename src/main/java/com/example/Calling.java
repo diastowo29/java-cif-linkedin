d46d7137d495e2354efbc67c83c98d1b74426f19
diastowo29 <diastowo@gmail.com>
2018-05-09 17:56:09 +0700

    add testing

---
+++ src/main/java/com/example/Calling.java
@@ -0,0 +1,44 @@
+package com.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Calling {
+	public String callingGet(String newUrl) {
+		String output = "";
+		try {
+			URL url = new URL(newUrl);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+			conn.setRequestProperty("Accept", "application/json");
+
+			if (conn.getResponseCode() != 200) {
+				throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
+			}
+
+			BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+
+			System.out.println("Output from Server .... \n");
+			while ((output = br.readLine()) != null) {
+				System.out.println(output);
+			}
+
+			conn.disconnect();
+
+		} catch (MalformedURLException e) {
+
+			e.printStackTrace();
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
+		}
+
+		return output;
+	}
+}
--- src/main/java/com/example/Linkedin.java
+++ src/main/java/com/example/Linkedin.java
@@ -8,12 +8,18 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Controller
 @SpringBootApplication
 @RequestMapping("/linkedin/")
 public class Linkedin {
 	String returnUrl = "";
+	String getAccessToken = "https://graph.facebook.com/v2.11/oauth/access_token?client_id=376575612769500&redirect_uri=https://cif-instagram.herokuapp.com/zendesk/instagram/mytoken/&client_secret=c95fc0a354beb66dc9bb490e85762ec3&code=";
+	String getAccounts = "";
 
 	@RequestMapping(method = RequestMethod.GET)
 	String indexGet() {
@@ -37,7 +43,27 @@ public class Linkedin {
 	@RequestMapping("/submit")
 	String submitToken(@RequestParam("token") String token) {
 		System.out.println("GET SUBMIT TOKEN: " + token);
-		return "getToken";
+		try {
+
+			ObjectMapper mapper = new ObjectMapper();
+			HashMap<String, String> response = new HashMap<>();
+			Calling calling = new Calling();
+			String output = calling.callingGet(getAccessToken + token);
+			response = mapper.readValue(output, new TypeReference<HashMap<String, String>>() {
+			});
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return "igAccounts";
+	}
+
+	@RequestMapping("/testing")
+	ModelAndView testingMethod() {
+		ModelAndView mav = new ModelAndView("testing");
+
+		return mav;
 	}
 
 }
