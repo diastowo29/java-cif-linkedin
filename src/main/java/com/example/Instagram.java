f1a23f8dc0cea4cf992ca079c03744d0b0fe2c83
diastowo29 <diastowo@gmail.com>
2018-06-05 13:41:50 +0700

    testing preflight

--- src/main/java/com/example/Instagram.java
+++ src/main/java/com/example/Instagram.java
@@ -14,12 +14,14 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @SpringBootApplication
+@CrossOrigin
 @RequestMapping("/instagram/")
 public class Instagram {
 	String herokuDomain = "https://java-cif-linkedin.herokuapp.com/";
