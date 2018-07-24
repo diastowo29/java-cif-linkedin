c589626d3dca6f5748e28ec02fa55f89246f1ba7
diastowo29 <diastowo@gmail.com>
2018-07-24 15:11:19 +0700

    add dtb

--- src/main/java/com/example/model/Comments.java
+++ src/main/java/com/example/model/Comments.java
@@ -10,7 +10,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "comments")
+@Table(name = "cif_comments")
 public class Comments implements Serializable {
 
 	private static final long serialVersionUID = 8814785481048416620L;
