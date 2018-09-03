b64619659d18b59bc039e04f3e81b8bb3a721608
diastowo29 <diastowo@gmail.com>
2018-09-03 15:19:14 +0700

    include caption

--- src/main/java/com/example/Instagram.java
+++ src/main/java/com/example/Instagram.java
@@ -369,8 +369,14 @@ public class Instagram {
 									allMedia.getJSONArray("data").getJSONObject(i).getString("media_url"));
 							displayInfo.put("type", "cif-media-" + parentMedia);
 							displayInfo.put("data", displayObject);
-
 							displayArray.add(displayInfo);
+							displayObject = new HashMap<>();
+							displayInfo = new HashMap<>();
+							displayObject.put("media_caption",
+									allMedia.getJSONArray("data").getJSONObject(i).getString("caption"));
+							displayInfo.put("type", "cif-caption-" + parentMedia);
+							displayInfo.put("data", displayObject);
+							
 
 							extObj.put("display_info", displayArray);
 							extObj.put("author", author);
@@ -405,8 +411,14 @@ public class Instagram {
 											allMedia.getJSONArray("data").getJSONObject(i).getString("media_url"));
 									displayInfo.put("type", "cif-comment-" + parentComment);
 									displayInfo.put("data", displayObject);
-
 									displayArray.add(displayInfo);
+									displayObject = new HashMap<>();
+									displayInfo = new HashMap<>();
+									displayObject.put("media_caption",
+											allMedia.getJSONArray("data").getJSONObject(i).getString("caption"));
+									displayInfo.put("type", "cif-caption-" + parentMedia);
+									displayInfo.put("data", displayObject);
+									
 									extObj.put("display_info", displayArray);
 
 									extObj.put("external_id", "cif-comment-" + parentComment);
@@ -455,10 +467,15 @@ public class Instagram {
 												displayInfo.put("type", "cif-comment-" + parentComment);
 											}
 											displayInfo.put("data", displayObject);
-
 											displayArray.add(displayInfo);
+											displayObject = new HashMap<>();
+											displayInfo = new HashMap<>();
+											displayObject.put("media_caption",
+													allMedia.getJSONArray("data").getJSONObject(i).getString("caption"));
+											displayInfo.put("type", "cif-caption-" + parentMedia);
+											displayInfo.put("data", displayObject);
+											
 											extObj.put("display_info", displayArray);
-
 											extObj.put("external_id",
 													"cif-comment-" + allMedia.getJSONArray("data").getJSONObject(i)
 															.getJSONObject("comments").getJSONArray("data")
